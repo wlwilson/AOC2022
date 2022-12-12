ed6121b6ea0dc069d2c2c2c83a8b7b6437babb08
Willy Wilson <willy.com@gmail.com>
2022-12-11 20:39:17 -0700

    added day 10

---
+++ 10/part1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+cycle=1
+x=1
+strength=0
+File.readlines('input').each do |ins|
+    puts "Cycle #{cycle}, X: #{x}, Strength: #{cycle*x}"
+    puts "instruction #{ins}"
+    case ins
+    when /noop/
+        cycle+=1
+    when /addx (-?\d+)/
+        puts "Adding #{$1}"
+        cycle+=1
+        puts "Cycle #{cycle}, X: #{x}, Strength: #{cycle*x}"
+        cycle+=1
+        #puts "Cycle #{cycle}, X: #{x}, Strength: #{cycle*x}"
+        x+=$1.to_i
+    end
+end
