50d1686c38f67a5dc0f4245462dbdddeb7f58b7c
Willy Wilson <willy.com@gmail.com>
2022-12-18 21:51:56 -0600

    Finished part 2!

--- 8/part2.rb
+++ 8/part2.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/ruby
 require 'pry'
-trees=File.readlines('test').map { |line| line.split(/\n|/).map{ |e| e.to_i}}
+#trees=File.readlines('test').map { |line| line.split(/\n|/).map{ |e| e.to_i}}
+trees=File.readlines('input').map { |line| line.split(/\n|/).map{ |e| e.to_i}}
 score=Array.new(trees.length) {Array.new(trees[0].length, 0)}
 
 def cnt_before_eqorgt(arr, val)
     count=0 
-    binding.pry
     unless arr.length==0 then
-        puts "Array length is #{arr.length} array at count is #{arr[count]} count is #{count} array is #{arr}"
-        binding.pry
-       while arr[count]<val && count < arr.length do
-           count+=1
-       end
+        #puts "Array length=#{arr.length} array@count=#{arr[count]} count=#{count} array=#{arr}"
+        #binding.pry
+        #while count < arr.length && arr[count]<val do
+        arr.each do |tree|
+            count+=1
+            if tree>=val then
+                break
+            end
+        end
     end
     return count
 end
@@ -20,29 +24,33 @@ def find_score(map, i, j)
     val=map[i][j]
     horiz=map[i]
     vert=map.transpose[j]
-    left=cnt_before_eqorgt(horiz[0,j],map[i][j])
-    #right=cnt_before_eqorgt(horiz[j+1,horiz.length],map[i][j])
-    #up=cnt_before_eqorgt(vert[0,i],map[i][j])
-    #down=cnt_before_eqorgt(vert[i+1,vert.length],map[i][j])
-    #score=left*right*up*down
-    score=left
+    up=cnt_before_eqorgt(vert[0,i].reverse,map[i][j])
+    #puts "Look up - score is #{up}"
+    left=cnt_before_eqorgt(horiz[0,j].reverse,map[i][j])
+    #puts "Look left - score is #{left}"
+    right=cnt_before_eqorgt(horiz[j+1,horiz.length],map[i][j])
+    #puts "Look right - score is #{right}"
+    down=cnt_before_eqorgt(vert[i+1,vert.length],map[i][j])
+    #puts "Look down - score is #{down}"
+    score=left*right*up*down
+    #puts "Total score is #{score}"
     return score
-    #binding.pry
 end
 
 perimiterTress=(trees.length-1)*(trees[0].length-1)
 trees.each_with_index do |arr, i|
     arr.each_with_index do |node, j|
-        puts "### checking map at #{i},#{j}"
+        #puts "### checking map at #{i},#{j}"
         score[i][j]=find_score(trees,i,j)
     end
 end
 
-for i in 0..trees.length() do
-    puts "#{score[i]}"
-end
-for i in 0..trees.length() do
-    puts "#{trees[i]}"
-end
+#for i in 0..trees.length() do
+#    puts "#{trees[i]}"
+#end
+#for i in 0..trees.length() do
+#    puts "#{score[i]}"
+#end
 
+puts "Max Score is #{score.map{|row| row.max}.max}"
 #puts "Sum is #{visible.map{|arr| arr.map{|e| e.to_i}.sum}.sum}"
