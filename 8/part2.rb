4da9104e577b2a91fc35c49a2d7111d2b0921a25
Willy Wilson <willy.com@gmail.com>
2022-12-18 19:59:15 -0700

    Still need to finish find_score...

--- 8/part2.rb
+++ 8/part2.rb
@@ -3,13 +3,37 @@ require 'pry'
 trees=File.readlines('test').map { |line| line.split(/\n|/).map{ |e| e.to_i}}
 score=Array.new(trees.length) {Array.new(trees[0].length, 0)}
 
+def cnt_before_eqorgt(arr, val)
+    count=0 
+    binding.pry
+    unless arr.length==0 then
+        puts "Array length is #{arr.length} array at count is #{arr[count]} count is #{count} array is #{arr}"
+        binding.pry
+       while arr[count]<val && count < arr.length do
+           count+=1
+       end
+    end
+    return count
+end
+
 def find_score(map, i, j)
     val=map[i][j]
+    horiz=map[i]
+    vert=map.transpose[j]
+    left=cnt_before_eqorgt(horiz[0,j],map[i][j])
+    #right=cnt_before_eqorgt(horiz[j+1,horiz.length],map[i][j])
+    #up=cnt_before_eqorgt(vert[0,i],map[i][j])
+    #down=cnt_before_eqorgt(vert[i+1,vert.length],map[i][j])
+    #score=left*right*up*down
+    score=left
+    return score
+    #binding.pry
 end
 
 perimiterTress=(trees.length-1)*(trees[0].length-1)
 trees.each_with_index do |arr, i|
     arr.each_with_index do |node, j|
+        puts "### checking map at #{i},#{j}"
         score[i][j]=find_score(trees,i,j)
     end
 end
